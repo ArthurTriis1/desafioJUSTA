83156ec3376fb923017db931bffd2e3cf4532a0b
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-06-02 11:24:44 -0300

    first adjusts

--- desafiojusta/src/Components/Dropdown/styles.ts
+++ desafiojusta/src/Components/Dropdown/styles.ts
@@ -2,18 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   select {
+    background: transparent;
     border-radius: 10px;
+    color: blue;
     font-family: "Press Start 2P", sans-serif;
-    font-size: 0.7rem;
+    font-size: 1rem;
+    height: 2.5rem;
+    margin: 2rem;
     max-width: 80vw;
     text-align: center;
   }
-
+  
   option:nth-child(even) {
     background: #fff;
   }
 
   option:nth-child(odd) {
     background: #ddd;
+  }
+
+  h1 {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    color: blue;
+    font-family: "Press Start 2P";
+    font-size: 1.2rem;
+    margin: 10px;
   }
 `;
